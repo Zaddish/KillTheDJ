c642572fee460b0caff035b249ffd0b9381e01f3
SUT0L <40848785+Zaddish@users.noreply.github.com>
2023-11-03 19:00:40 +1100

    can now reattach if Spotify closes

--- Program.cs
+++ Program.cs
@@ -7,47 +7,69 @@ namespace SpotifyAutomator {
         private const int APPCOMMAND_MEDIA_NEXTTRACK = 11;
         private const int WM_APPCOMMAND = 0x0319;
         private static WinEventDelegate dele = null;
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_OBJECT_NAMECHANGE = 0x800C;
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
 
         [DllImport("user32.dll")]
         public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
-
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-
         [DllImport("user32.dll")]
         public static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
-
         [DllImport("user32.dll")]
         static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
-
-        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
-
         [DllImport("user32.dll")]
         public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+        [DllImport("user32.dll")]
+        static extern bool UnhookWinEvent(IntPtr hWinEventHook);
 
-        private const uint WINEVENT_OUTOFCONTEXT = 0;
-        private const uint EVENT_OBJECT_NAMECHANGE = 0x800C;
 
         private enum SpotifyState {
             Paused,
             Playing,
-            Resumed
+            Resumed,
+            Closed
         }
 
         private static SpotifyState currentState = SpotifyState.Paused;
         private static string lastPlayedTitle = "";
         private static Dictionary<IntPtr, string> spotifyWindowHandles = new Dictionary<IntPtr, string>();
+        private static IntPtr m_hhook = IntPtr.Zero;
 
         static void Main(string[] args) {
+            
             Process[] spotifyProcesses = Process.GetProcessesByName("Spotify");
             if (spotifyProcesses.Length > 0) {
                 uint spotifyProcessId = (uint)spotifyProcesses[0].Id; // If it's not the first process get rekt bozo lol
+                var process = Process.GetProcessById((int)spotifyProcessId);
+                if (process != null && process.ProcessName.Equals("Spotify", StringComparison.OrdinalIgnoreCase)) {
+                    process.EnableRaisingEvents = true;
+                    process.Exited += (sender, args) => {
+                        currentState = SpotifyState.Closed;
+                        while (true) {
+                            if (currentState == SpotifyState.Closed) {
+                                Console.WriteLine("Spotify has been closed, waiting for it to restart...");
+                                Process[] processes;
+                                do {
+                                    Thread.Sleep(1000);
+                                    processes = Process.GetProcessesByName("Spotify");
+                                } while (processes.Length == 0);
+
+                                Console.WriteLine("Spotify has restarted, reinitializing...");
+                                Main(new string[] { });
+                            }
+                        }
+                    };
+                }
                 dele = new WinEventDelegate(WinEventProc);
                 IntPtr m_hhook = SetWinEventHook(EVENT_OBJECT_NAMECHANGE, EVENT_OBJECT_NAMECHANGE, IntPtr.Zero, dele, spotifyProcessId, 0, WINEVENT_OUTOFCONTEXT);
                 InitializeSpotifyState(spotifyProcessId);
                 Application.Run();
             } else {
-                Console.WriteLine("Spotify is not running.");
+                Console.WriteLine("Spotify is not running. Please open spotify before opening the DJ Killer");
+                Console.ReadKey();
                 Application.Exit();
             }
         }
